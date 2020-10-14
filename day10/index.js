c1fdb11f4eb701d95eff6b3dfe741c90af9890a4
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-10-14 16:12:30 +0900

    random number game

---
+++ day10/index.js
@@ -0,0 +1,46 @@
+const number = document.querySelector(".js-number");
+const slider = document.querySelector(".js-slider"),
+    inputSlider = slider.querySelector("input");
+const chooseNum = document.querySelector(".chooseNum"),
+    playBtn = document.querySelector(".playBtn");
+const result = document.querySelector(".result"),
+    gameResult = document.querySelector(".gameResult");
+
+
+
+function winLost(n, ran) {
+    if (n === ran) {
+        gameResult.innerHTML = "You win!";
+    } else {
+        gameResult.innerHTML = "You lost!";
+    }
+}
+
+function randomNum(min, max, n) {
+    ranNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    result.innerHTML = `You chose: ${n}, the machine chose: ${ranNum}`;
+    winLost(n, ranNum);
+}
+
+function playGame(n) {
+    const range = inputSlider.value;
+    if (Number(n) > Number(range)) {
+        alert(`You can't over ${range}`);
+    } else {
+        randomNum(0, range, n);
+    }
+}
+
+
+function rangeChange() {
+    const range = inputSlider.value;
+    number.innerText = `Generate a number between 0 and ${range}`;
+    chooseNum.max = range;
+}
+
+function init() {
+    inputSlider.addEventListener("mousemove", rangeChange);
+    playBtn.addEventListener("click", function () { playGame(chooseNum.value) });
+}
+
+init();

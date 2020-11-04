aa9b86a93627c8ceb5f04a909029643ff18ba4c7
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-04 22:39:57 +0900

    get date

--- 00-application/clock.js
+++ 00-application/clock.js
@@ -1,18 +1,30 @@
 const clockContainer = document.querySelector(".js-clock"),
+    dateTitle = clockContainer.querySelector("h3"),
     clockTitle = clockContainer.querySelector("h1");
+
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 function getTime(){
     const date = new Date();
     const minutes = date.getMinutes(),
-        hours = date.getHours(),
-        seconds = date.getSeconds();
+        hours = date.getHours();
     clockTitle.innerText = `${hours>9 ? hours:`0${hours}`}:${
         minutes>9 ? minutes:`0${minutes
-        }`}:${seconds>9 ? seconds:`0${seconds}`}`;
+        }`}`;
+}
+
+function getDate(){
+    const nowDate = new Date();
+    const day = nowDate.getDay(),
+        date = nowDate.getDate(),
+        month = nowDate.getMonth(),
+        year = nowDate.getFullYear();
+    dateTitle.innerText = `${year}.${month}.${date} ${days[day]}`
 }
 
 function init(){
     setInterval(getTime,1000);
+    getDate();
 }
 
 init();

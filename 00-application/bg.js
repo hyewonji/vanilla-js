7aacc1443ffb01a7ec0fe4e388589e7459d06d44
hyewonji <60416187+hyewonji@users.noreply.github.com>
2020-11-06 14:58:54 +0900

    .

--- 00-application/bg.js
+++ 00-application/bg.js
@@ -1,24 +1,24 @@
 const body = document.querySelector("body");
 
-const IMG_NUMBER = 18;
+const IMG_NUMBER = 14;
 
-function handleImgLoad(){
+function handleImgLoad() {
     console.log("finished loading");
 }
 
-function paintImage(imgNumber){
+function paintImage(imgNumber) {
     const image = new Image();
-    image.src=`/images/${imgNumber+1}.JPG`;
+    image.src = `/images/${imgNumber + 1}.JPG`;
     image.classList.add("bgImage");
     body.appendChild(image);
 }
 
-function genRandom(){
+function genRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
     return number;
 }
 
-function init(){
+function init() {
     const randomNumber = genRandom();
     paintImage(randomNumber);
 }
